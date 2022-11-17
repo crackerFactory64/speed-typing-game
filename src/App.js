e7928895c939f6505c13f5512693df89e377bac9
crackerFactory <lee.mander@gmail.com>
2022-11-17 11:10:13 +0000

    mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const gameTime = 5;
+
+  const [timeRemaining, setTimeRemaining] = useState(gameTime);
+  const [isGameRunning, setIsGameRunning] = useState(false);
+  const [text, setText] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    if (isGameRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isGameRunning]);
+
+  const startGame = () => {
+    setTimeRemaining(gameTime);
+    setIsGameRunning(true);
+    setText("");
+    textareaRef.current.disabled = false;
+    textareaRef.current.focus();
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  const countWords = (text) => {
+    const textCopy = text;
+    setWordCount(textCopy ? textCopy.trim().split(" ").length : 0);
+  };
+
+  const endGame = () => {
+    setIsGameRunning(false);
+    countWords(text);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How fast do you type?</h1>
+      <textarea
+        ref={textareaRef}
+        value={text}
+        onChange={handleChange}
+        disabled={!isGameRunning}
+      />
+      <p>Time remaining: {timeRemaining}</p>
+      <button onClick={startGame} disabled={isGameRunning}>
+        Start
+      </button>
+      <p>Word count: {wordCount} </p>
     </div>
   );
 }
